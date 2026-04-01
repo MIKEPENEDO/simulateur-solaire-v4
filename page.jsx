ad2a703d33da5f45e476edb49592346b8903be1f
MIKEPENEDO <info@consulting-energies.fr>
2026-04-01 13:34:05 +0200

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>🚀 Simulateur solaire V4</h1>
+      <p>Application en ligne opérationnelle</p>
+    </main>
+  );
+}
